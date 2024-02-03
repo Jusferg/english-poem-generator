e6837bce79de696b5550b2c1b9b255b4dca9c53c
Tia <90842451+Jusferg@users.noreply.github.com>
2024-02-02 19:57:26 -0500

    added axios and api key to poem generator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
+    console.log("#poem generated");
 
 new Typewriter("#poem-finder", {
-    strings: "roses are red, violets are blue",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
 });
+}
+
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "e0afd0e3efa2a264e399b2e7eboa411t";
+   
+    let prompt = `Generate an English poem about ${instructionsInput.value}`;
+    let context = `You are a renaissance poet and love to write poems about the past. Your mission is to generate a poem from a set time in HTML. Please follow the user instructions`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    console.log("Generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+
+
+    axios.get(apiUrl).then(displayPoem);
+
 }
 
 let poemFormElement = document.querySelector("#poem-form-generator");

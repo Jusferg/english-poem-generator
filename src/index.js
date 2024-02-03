0ce4b16e58d2d663b92ae4dfd29ad3cd6e7d8d1a
Tia <90842451+Jusferg@users.noreply.github.com>
2024-02-02 20:28:50 -0500

    improved user experience

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function displayPoem(response) {
     console.log("#poem generated");
 
-new Typewriter("#poem-finder", {
+new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
@@ -17,8 +17,12 @@ function generatePoem(event) {
     let apiKey = "e0afd0e3efa2a264e399b2e7eboa411t";
    
     let prompt = `Generate an English poem about ${instructionsInput.value}`;
-    let context = `You are a renaissance poet and love to write poems about the past. Your mission is to generate a poem from a set time in HTML. Please follow the user instructions`;
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+    let context = `You are a renaissance poet and love to write poems about the past. Your mission is to generate a short poem from a set time in HTML. Please follow the user instructions`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating"> Generating the renaissance poem about ${instructionsInput.value} </div>`;
 
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
